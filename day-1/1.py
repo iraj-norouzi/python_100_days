77943e2a9da2dc3c52df321abcbc8a11b4eac954
Iraj <useriraj2000@gmail.com>
2023-07-19 12:16:43 +0330

    Day-1

---
+++ day-1/1.py
@@ -0,0 +1,20 @@
+# comments and documentation
+# print(len(input("what is your name?")))
+# name = len(input("what is your name?"))
+
+# name = input("what is your name?")
+# print(name)
+# length = len(name)
+# print(length)
+
+
+a = input("number-a: ")
+b = input("number-b: ")
+
+
+c = a
+a = b
+b = c
+
+print("number-a= " + a)
+print("number-b= " + b)
